e28d4aabd045029a7d8bb3464731bb34f2cf19df
Ashish <ashisprusty71@gmail.com>
2023-11-15 20:43:56 +0530

    adding dynamic routing

---
+++ app/studentlist/[student]/page.jsx
@@ -0,0 +1,13 @@
+"use client"
+import React from 'react'
+
+const StudentDetails = ({params}) => {
+    console.log('====================================');
+    console.log(params);
+    console.log('====================================');
+  return (
+    <div>StudentDetails1</div>
+  )
+}
+
+export default StudentDetails
