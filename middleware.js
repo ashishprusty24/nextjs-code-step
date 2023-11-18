09bf4b1bb256918ac1770973e8e81c4f55a4f599
Ashish <ashisprusty71@gmail.com>
2023-11-18 10:53:56 +0530

    adding middileware code

---
+++ middleware.js
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server";
+
+export const middleware = (request)=>{
+    console.log("middileware");
+
+    // if(request.nextUrl.pathname!="/login"){
+        return NextResponse.redirect(new URL("/login",request.url) )
+    // }
+}
+
+export const config = {
+    matcher: "/about/:path"
+}
