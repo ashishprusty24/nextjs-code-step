d93de01f4f8e3333894952818fe400a2e6573183
Ashish <ashisprusty71@gmail.com>
2023-11-19 20:41:14 +0530

    adding style and image optimisation code

---
+++ app/user/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+// import custom from "../custom.module.css"
+import custom from "@/style/user.module.css"
+const User = () => {
+  return (
+    <div className={custom.main} >User</div>
+  )
+}
+
+export default User
