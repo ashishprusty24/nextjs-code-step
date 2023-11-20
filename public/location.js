31a2e448312915bda76405ec24228432b85d80f6
Ashish <ashisprusty71@gmail.com>
2023-11-20 20:53:07 +0530

    adding script file

---
+++ public/location.js
@@ -0,0 +1,16 @@
+function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else { 
+      x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+  }
+  
+  function showPosition(position) {
+    console.log(position.coords.latitude);
+    console.log(position.coords.longitude);
+    x.innerHTML = "Latitude: " + position.coords.latitude + 
+    "<br>Longitude: " + position.coords.longitude;
+  }
+
+  getLocation() 
